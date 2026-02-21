e6bc568ebaf3110236417a7fff6af25a45ec3530
Ridikas Batonas <hello@karolisram.com>
2026-02-21 05:41:31 -0800

    build: add prettier and eslint-config-prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 100,
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./app/globals.css",
+};
+
+export default config;
